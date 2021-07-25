0c07897f1e4f78f942dd831b7555e2329be98d91
Robert Rousset <whyisrob@hotmail.com>
2021-07-25 21:25:38 +0800

    added projects page skelleton

---
+++ src/components/Projects.js
@@ -0,0 +1,35 @@
+export default function Projects() {
+  return (
+    <section id="work">
+      <h2>My Work</h2>
+
+      <article class="work">
+        <div class="cardBigger">
+          {/* <a href="https://robert-rousset.github.io/MMM-Farm-App/" target="_blank"><img class="mainProjectImage"
+                        src="./Images/Project Screenshots/Project1-Screenshot.png" alt="woo"></a> */}
+          <p>Project1</p>
+        </div>
+        <div class="workCard">
+          {/* <a href="https://robert-rousset.github.io/Weather-Dashboard/" target="_blank"><img class="otherImage"
+                        src="./Images/Project Screenshots/Weather-Dashboard-Screenshot.png" alt="woo"></a> */}
+          <p>Weather Dashboard</p>
+        </div>
+        <div class="workCard">
+          {/* <a href="https://robert-rousset.github.io/Daily-Planner/" target="_blank"><img class="otherImage"
+                        src="./Images/Project Screenshots/Work-Day-Scheduler.png" alt="woo"></a> */}
+          <p>Work Day Scheduler</p>
+        </div>
+        <div class="workCard">
+          {/* <a href="https://robert-rousset.github.io/Robs-Quiz-Game/" target="_blank"><img class="otherImage"
+                        src="./Images/Project Screenshots/Quiz-Screenshot.png" alt="woo"></a> */}
+          <p>Quiz Game</p>
+        </div>
+        <div class="workCard">
+          {/* <a href="https://robert-rousset.github.io/Password-Generator/" target="_blank"><img class="otherImage"
+                        src="./Images/Project Screenshots/Password-Generator-Screenshot.png" alt="woo"></a> */}
+          <p>Password Generator</p>
+        </div>
+      </article>
+    </section>
+  );
+}
