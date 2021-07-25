b33d43e05a7e2c38c714bb9bdfe75f7d73bcf3a9
Robert Rousset <whyisrob@hotmail.com>
2021-07-25 21:53:45 +0800

    added logos to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,26 @@
+import phone from "../img/logos/phone.png";
+import email from "../img/logos/email.png";
+import github from "../img/logos/github.png";
+import linkedin from "../img/logos/linkedin.png";
+
 export default function Contact() {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
       <article class="contact">
         <div class="contactImgBox">
-          {/* <img src="Images/Buttons/phone.png"> */}
+          <img src={phone} />
           <a href="https://www.gmail.com" target="_blank">
-            {/* <img src="Images/Buttons/email.png"> */}
+            <img src={email} />
           </a>
           <a href="https://github.com/Robert-Rousset" target="_blank">
-            {/* <img src="Images/Buttons/github.png"> */}
+            <img src={github} />
           </a>
           <a
             href="https://www.linkedin.com/in/robert-rousset-3a8300193/"
             target="_blank"
           >
-            {/* <img src="Images/Buttons/linkedin.png"> */}
+            <img src={linkedin} />
           </a>
         </div>
 
